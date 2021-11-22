b995f8d06b62ac8e1692d7ad688c8fbed1176f73
Michael <kipsang20323@gmail.com>
2021-11-22 00:04:59 -0700

    finalize and add README

--- index.js
+++ index.js
@@ -1,70 +1,80 @@
 import neatCsv from 'neat-csv';
-import { parse } from 'csv';
-import csvParser from 'csv-parser';
 import { createObjectCsvWriter } from 'csv-writer';
-import fs from 'fs';
 import * as fsPromises from 'fs/promises';
-import path, { dirname } from 'path';
 
+async function generateOutput() {
+    const [,,modes] = process.argv;
+    
+    const { 
+        CSV_FILE_PATH, TOP_POSTS_PATH, OTHER_POSTS_PATH, DAILY_TOP_POSTS_PATH
+    } = getConstants();
 
-const CSV_FILE_PATH = 'posts.csv';
-const TOP_POSTS_PATH = 'top_posts.csv';
-const OTHER_POSTS_PATH = 'other_posts.csv';
-const DAILY_TOP_POSTS_PATH = 'daily_top_posts.csv';
-
-const PUBLIC = 'public';
-
-(async function init() {
     const csvFile = await fsPromises.readFile(CSV_FILE_PATH, 'utf-8');
     const postList = await neatCsv(csvFile);
-    const formattedData = postList.reduce((output, post) => {
-        return post.timestamp ? output.concat(post) : output.concat(fixData(post));
-    }, []);
-    const output = generatePostLists(formattedData);
-    console.log('output: ', output);
-    const { topPosts, otherPosts, dailyTopPosts } = output;
+    const formattedData = formatData(postList);
+    const postOutput = generatePostLists(formattedData);
+    const { topPosts, otherPosts, dailyTopPosts } = postOutput;
     const dailyTopPostValues = Object.values(dailyTopPosts);
 
-    // const topPostsWrite = fsPromises.writeFile(TOP_POSTS_PATH, topPosts);
-    const topPostsWrite = writeCsv(topPosts, TOP_POSTS_PATH);
-    const otherPostsWrite = writeCsv(otherPosts, OTHER_POSTS_PATH);
-    const dailyTopPostsWrite = writeCsv(dailyTopPostValues, DAILY_TOP_POSTS_PATH);
-    // const otherPostsWrite = fsPromises.writeFile(OTHER_POSTS_PATH, otherPosts);
+    const topPostsWrite = writeOutputFile(topPosts, TOP_POSTS_PATH, modes);
+    const otherPostsWrite = writeOutputFile(otherPosts, OTHER_POSTS_PATH, modes);
+    const dailyTopPostsWrite = writeOutputFile(dailyTopPostValues, DAILY_TOP_POSTS_PATH, modes);
     const writeRequests = [topPostsWrite, otherPostsWrite, dailyTopPostsWrite];
     await Promise.all(writeRequests);
-    // BONUS:
-    // output json
-    // output full record, not just id
+}
 
-})();
+function formatData(postList) {
+    return postList.reduce((output, post) => post.timestamp ? output.concat(post) : output.concat(adjustPost(post)), []);
+}
 
 /**
- * Csv parser is not correctly escaping double quotes
+ * Csv parser is not correctly escaping double quotes; this method 
+ * correctly formats the non-escaped rows
  * Ex of badly parsed data: {
     id: '4839504',
     title: `Funniest "I Can't Go to Bed Yet" Excuses,private,73,16735,11,Sat Oct 12 06:10:48 2015`
   },
  * @param {*} post 
  */
-function fixData(post) {
+function adjustPost(post) {
     const { id, title: incorrectlyFormattedTitle } = post;
     const [title, privacy, likes, views, comments, timestamp] = incorrectlyFormattedTitle.split(',');
     return { id, title, privacy, likes, views, comments, timestamp};
 }
 
-async function writeCsv(postList, path) {
-    return createObjectCsvWriter({
-        path,
-        header: [
-            {id: 'id'}
-        ]
-    })
-    .writeRecords(postList);
+
+async function writeOutputFile(postList, path, modes) {
+    let jsonMode = false;
+    let detailMode = false;
+    if (modes) {
+        jsonMode = modes.includes('j');
+        detailMode = modes.includes('d');
+    }
+
+    if (jsonMode) {
+        const postData = detailMode ? postList : postList.map(post => post.id);
+        return fsPromises.writeFile(`${path}.json`, JSON.stringify(postData));
+    } else {
+        let header = [{id: 'id', title: 'id'}];
+        if (detailMode) {
+            const additionalFields = [
+                {id: 'title', title: 'title'},
+                {id: 'privacy', title: 'privacy'},
+                {id: 'likes', title: 'likes'},
+                {id: 'views', title: 'views'},
+                {id: 'comments', title: 'comments'},
+                {id: 'timestamp', title: 'timestamp'}
+            ];
+            header = header.concat(...additionalFields);
+        }
+        return createObjectCsvWriter({path: `${path}.csv`,header}).writeRecords(postList);
+    }
 }
 
 function generatePostLists(postList) {
+    const { PUBLIC } = getConstants();
     return postList.reduce((lists, post) => {
-        const { id, privacy, views, comments, title, likes, timestamp } = post;
+        const { privacy, views, comments, title, likes, timestamp } = post;
         const date = parseDate(timestamp);
         const isTopPost = (privacy === PUBLIC) && (title.length < 40) && (+comments > 10) && (+views > 9000); 
         if (isTopPost) {
@@ -72,14 +82,8 @@ function generatePostLists(postList) {
         } else {
             lists.otherPosts.push(post);
         }
-        
         const isNewDay = !lists.dailyTopPosts[date]; // if current date has no posts yet, its first post automatically becomes the most-liked
         const currentPostHasMoreLikes = lists.dailyTopPosts[date] && +lists.dailyTopPosts[date].likes < +likes;
-        // if (date === 'Invalid Date') {
-        //     console.log('invalid post: ', post);
-        //     console.log('teimstapm: ', timestamp);
-        //     console.log('date: ', date);
-        // }
         if (isNewDay || currentPostHasMoreLikes) {
             lists.dailyTopPosts[date] = post;
         }
@@ -96,3 +100,14 @@ function parseDate(timestamp) {
     return date.toLocaleDateString();
 };
 
+function getConstants() {
+    return {
+        CSV_FILE_PATH: 'posts.csv',
+        TOP_POSTS_PATH: 'top_posts',
+        OTHER_POSTS_PATH: 'other_posts',
+        DAILY_TOP_POSTS_PATH: 'daily_top_posts',
+        PUBLIC: 'public'
+    };
+}
+
+generateOutput();
